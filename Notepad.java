8cff3d79074485d5281b0da6f429bebdbc92dd2d
Garnt <ghkdtnsxo321@gmail.com>
2022-11-23 17:57:27 +0900

    change main

--- Notepad.java
+++ Notepad.java
@@ -20,26 +20,16 @@ public class Notepad {
 
             switch (choice) {
                 case 1:
-                    System.out.println("이름을 입력해주세요.");
-                    String name = sc.nextLine();
-                    System.out.println("비밀번호를 입혁해주세요.");
-                    String password = sc.nextLine();
-                    System.out.println("메모를 입력해주세요.");
-                    String text = sc.nextLine();
-                    memoAdd(name, password, text);
+                    memoAdd();
                     break;
                 case 2:
                     memoList();
                     break;
                 case 3:
-                    System.out.println("글 번호를 입력해주세요.");
-                    int index = sc.nextInt();
-                    memoEdit(index);
+                    memoEdit();
                     break;
                 case 4:
-                    System.out.println("글 번호를 입력해주세요.");
-                    index = sc.nextInt();
-                    memoDel(index);
+                    memoDel();
                     break;
                 case 5:
                     System.out.println("사용 종료");
@@ -53,23 +43,5 @@ public class Notepad {
             }
             if (flag == true) break;
         }
-    }
-
-    private static void memoDel(int index) {
-        System.out.println(index + " 삭제");
-    }
-
-    private static void memoEdit(int index) {
-        System.out.println(index + " 수정");
-    }
-
-    private static void memoList() {
-        System.out.println("목록");
-    }
-
-    private static void memoAdd(String name, String password, String text) {
-        System.out.println(name);
-        System.out.println(password);
-        System.out.println(text);
     }
 }
