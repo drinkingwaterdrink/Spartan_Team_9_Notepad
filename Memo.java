14cbc8caab68783a7a83140f751ca8e0126f25f6
drinkingwaterdrink <118874739+drinkingwaterdrink@users.noreply.github.com>
2022-11-23 18:02:42 +0900

    Update Memo.java

--- Memo.java
+++ Memo.java
@@ -2,7 +2,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Memo {
-
+    //
     private String name;
      private String password;
     String text;
